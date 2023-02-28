56114772f0c0792aa78b93095c0a07435aced342
Thatcrazygirl8 <francatherin03@gmail.com>
2023-02-27 19:35:22 -0800

    Don't swap horses in crossing a stream

--- 0x05-pointers_arrays_strings/1-main.c
+++ 0x05-pointers_arrays_strings/1-main.c
@@ -1,14 +1,25 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
  */
 
-void reset_to_98(int *a)
+void swap_int(int *a, int *b)
 
 {
-	*a = 98;
+
+	int m;
+
+
+	m = *a;
+
+	*a = *b;
+
+	*b = m;
+
 }
+
+
+
